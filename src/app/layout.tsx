cfa407ce1afd1c7a3d973a8826f98213021ec897
Maciek Skokowski <maciejskok2004@gmail.com>
2024-03-11 01:05:05 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,16 +13,16 @@ export default function RootLayout({
   return (
 		<html lang='en' className="bg-[#050505]">
 			<head>
-				<title>Portfolio | Maciek Skokowski</title>
+				<title>Portfolio | Maciek</title>
 				<meta name='theme-color' content='#00b894' />
 				<meta property='og:image' content='/next.svg' />
 				<meta
 					name="keywords"
-					content='vvaciej, maciek skokowski, maciej skokowski, skokowski, vvaciej portfolio, maciek portfolio, maciej portfolio, skokowski portfolio, frontend developer, klepacz kodu'
+					content='vvaciej, maciek, maciej, vvaciej portfolio, maciek portfolio, maciej portfolio, portfolio, frontend developer, klepacz kodu'
 				/>
 				<meta
 					name='description'
-					content='Portfolio Front-end Developera - Maciek Skokowski'
+					content='Portfolio Front-end Developera - Maciek'
 				/>
 			</head>
 			<body className={inter.className}>{children}</body>
